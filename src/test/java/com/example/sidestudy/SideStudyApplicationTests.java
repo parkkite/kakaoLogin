8fe8f00a485c9e49d83ff411a7787421a4233fe3
박기태 <76659923+parkkite@users.noreply.github.com>
2024-07-03 04:13:05 +0900

    create project

---
+++ src/test/java/com/example/sidestudy/SideStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sidestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
