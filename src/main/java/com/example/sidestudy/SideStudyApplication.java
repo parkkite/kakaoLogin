543d41124d5b665de4c9cc630444823d3be74368
박기태 <76659923+parkkite@users.noreply.github.com>
2024-07-03 04:10:17 +0900

    crate project

---
+++ src/main/java/com/example/sidestudy/SideStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.sidestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SideStudyApplication.class, args);
+    }
+
+}
