3687a73f9ceeca890a98f309c62d9d8b129de414
parkkitae <devkit94@gmail.com>
2024-07-05 04:07:38 +0900

    feature: MainController 수정

--- src/main/java/com/example/sidestudy/Controller/MainController.java
+++ src/main/java/com/example/sidestudy/Controller/MainController.java
@@ -2,15 +2,22 @@ package com.example.sidestudy.Controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class MainController {
 
-
     @GetMapping("/api1")
-    public ResponseEntity api1(){
+    public ResponseEntity api1(Model model){
         return new ResponseEntity<>("로그인 성공 화면입니다.", HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
     }
 }
