1cce58820e5fc68d4a17b60a9e476f74020223af
parkkitae <devkit94@gmail.com>
2024-07-05 04:08:53 +0900

    feature: OAuth 설정

--- src/main/java/com/example/sidestudy/WebSecurityConfig.java
+++ src/main/java/com/example/sidestudy/WebSecurityConfig.java
@@ -1,37 +1,64 @@
 package com.example.sidestudy;
 
+import com.example.sidestudy.Service.OAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-@RequiredArgsConstructor
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+    private final OAuth2UserService oAuth2UserService;
+
+    public WebSecurityConfig(OAuth2UserService oAuth2UserService) {
+        this.oAuth2UserService = oAuth2UserService;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
-        http.authorizeHttpRequests((auth)->
-                        auth
-                        .requestMatchers("/api1").hasRole("user")
-                .anyRequest().authenticated()
-        )
-                .formLogin((login) ->
-                        login
-                                .usernameParameter("username")
-                                .passwordParameter("password")
-                                .defaultSuccessUrl("/api1",true)
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(config -> config.anyRequest().permitAll())
+                .oauth2Login(oauth2Configurer->oauth2Configurer
+                        .successHandler(successHandler())
+                        .userInfoEndpoint(userInfoEndpoint ->userInfoEndpoint.userService(oAuth2UserService))
                 );
         return http.build();
     }
 
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        return ((request, response, authentication) -> {
+            DefaultOAuth2User defaultOAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
+
+            String id = defaultOAuth2User.getAttributes().get("id").toString();
+            String body = """
+                    {"id":"%s"}
+                    """.formatted(id);
+
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+            PrintWriter writer = response.getWriter();
+            writer.println(body);
+            writer.flush();
+        });
+    }
     @Bean
     public UserDetailsService userDetailsService(){
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
